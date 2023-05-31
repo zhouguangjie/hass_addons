cd1c4ec9d7413aa5186b69dc80737402cb83b552
zhouguangjie <zhou.guangjie@outlook.com>
2023-05-31 12:04:08 +0800

    update

---
+++ ddns/run.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/with-contenv bashio
+DDNS_DNS="$(bashio::config 'dns')"
+DDNS_ID="$(bashio::config 'id')"
+DDNS_TOKEN="$(bashio::config 'token')"
+DDNS_IPV4="$(bashio::config 'ipv4')"
+DDNS_IPV6="$(bashio::config 'ipv6')"
+DDNS_INDEX4="$(bashio::config 'index4')"
+DDNS_INDEX6="$(bashio::config 'index6')"
+DDNS_TTL="$(bashio::config 'ttl')"
+DDNS_PROXY="$(bashio::config 'proxy')"
+DDNS_DEBUG="$(bashio::config 'debug')"
+DDNS_CACHE="$(bashio::config 'cache')"
+exec /ddns
